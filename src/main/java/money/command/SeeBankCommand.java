cc30449124c38923c5f1ddf9cb571bc88e5249f2
Him188 <Him188@mamoe.net>
2017-08-07 00:35:35 +0800

    New commands

---
+++ src/main/java/money/command/SeeBankCommand.java
@@ -0,0 +1,60 @@
+package money.command;
+
+import cn.nukkit.Player;
+import cn.nukkit.command.Command;
+import cn.nukkit.command.CommandSender;
+import cn.nukkit.command.data.CommandParameter;
+import money.Money;
+import money.Utils;
+
+import java.util.HashMap;
+import java.util.Objects;
+
+/**
+ * @author Him188 @ Money Project
+ */
+public class SeeBankCommand extends MoneyCommand {
+    public SeeBankCommand(String name, Money owner, String[] aliases) {
+        super(name, owner, aliases);
+        this.setPermission("money.command.seebank");
+        this.setCommandParameters(new HashMap<String, CommandParameter[]>() {
+            {
+                put("see-bank", new CommandParameter[]{
+                        new CommandParameter("player", CommandParameter.ARG_TYPE_RAW_TEXT, false)
+                });
+            }
+        });
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!this.testPermission(sender)) {
+            //sender.sendMessage(this.getPlugin().translateMessage("has-no-permission"));
+            return true;
+        }
+
+        if (args.length < 1) {
+            sender.sendMessage(this.getPlugin().translateMessage("see-bank-format-error", "cmd", this.getName()));
+            return true;
+        }
+
+        Player p = Utils.getPlayer(args[0]);
+        String name;
+        if (p == null) {
+            name = args[0];
+        } else {
+            name = p.getName();
+        }
+
+        if (Objects.equals(name, "")) {
+            sender.sendMessage(this.getPlugin().translateMessage("see-bank-format-error", "cmd", this.getName()));
+            return true;
+        }
+
+        sender.sendMessage(this.getPlugin().translateMessage("see-bank-success",
+                "player", name,
+                "amount", Money.getInstance().getMoney(name)
+        ));
+        return false;
+    }
+}
---
+++ src/main/java/money/command/SeeMoney1Command.java
@@ -0,0 +1,61 @@
+package money.command;
+
+import cn.nukkit.Player;
+import cn.nukkit.command.Command;
+import cn.nukkit.command.CommandSender;
+import cn.nukkit.command.data.CommandParameter;
+import money.Money;
+import money.Utils;
+
+import java.util.HashMap;
+import java.util.Objects;
+
+/**
+ * @author Him188 @ Money Project
+ */
+public class SeeMoney1Command extends MoneyCommand {
+    public SeeMoney1Command(String name, Money owner, String[] aliases) {
+        super(name, owner, aliases);
+        this.setPermission("money.command.see1");
+        this.setCommandParameters(new HashMap<String, CommandParameter[]>() {
+            {
+                put("see-money-1", new CommandParameter[]{
+                        new CommandParameter("player", CommandParameter.ARG_TYPE_RAW_TEXT, false)
+                });
+            }
+        });
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!this.testPermission(sender)) {
+            //sender.sendMessage(this.getPlugin().translateMessage("has-no-permission"));
+            return true;
+        }
+
+        if (args.length < 1) {
+            sender.sendMessage(this.getPlugin().translateMessage("see-money-format-error", "cmd", this.getName()));
+            return true;
+        }
+
+        Player p = Utils.getPlayer(args[0]);
+        String name;
+        if (p == null) {
+            name = args[0];
+        } else {
+            name = p.getName();
+        }
+
+        if (Objects.equals(name, "")) {
+            sender.sendMessage(this.getPlugin().translateMessage("see-money-format-error", "cmd", this.getName()));
+            return true;
+        }
+
+        sender.sendMessage(this.getPlugin().translateMessage("see-money-success",
+                "player", name,
+                "amount", Money.getInstance().getMoney(name),
+                "type", Money.getInstance().getCurrency1()
+        ));
+        return false;
+    }
+}
